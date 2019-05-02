655b66c3940d0fecd883d6284472416b25e7b295
Alan Salkanovic <zekija816@gmail.com>
2019-05-02 15:33:58 -0500

    added micropost model and changed index page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'users#index'
   #root 'application#hello'
-  root 'application#chankoNabe'
+  #root 'application#chankoNabe'
+  root 'microposts#index'
 end
